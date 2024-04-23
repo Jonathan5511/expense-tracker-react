4626ffa9c03e3655ac84f9dda0b5315f3005f5c8
Jonathan5511 <jonathanpereira5511@gmail.com>
2024-04-23 07:54:18 +0530

    edit user details buy displaying data that is present in the database.

--- src/pages/UpdateProfile.js
+++ src/pages/UpdateProfile.js
@@ -1,4 +1,4 @@
-import { useRef,useContext } from 'react';
+import { useRef,useContext, useEffect } from 'react';
 import classes from './UpdateProfile.module.css'
 import AuthContext from '../store/auth-context';
 
@@ -6,6 +6,34 @@ const UpdateProfile=(props)=>{
     const authCtx = useContext(AuthContext)
     const nameInputRef=useRef()
     const imageInputRef=useRef()
+
+    useEffect(()=>{
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyA3Xrnzk0TTuA1haVoAVWYDaK2TtED9yTk',{
+            method:'POST',
+            body:JSON.stringify({
+                idToken:authCtx.token
+            }),
+            headers:{'Content-Type':'application/json'}
+        }).then(res=>{
+            if(res.ok){
+                return res.json()
+            }else{
+                return res.json().then((data)=>{
+                    let errorMessage= 'Authentication failed!'
+                    if(data && data.error && data.error.message){
+                        errorMessage = data.error.message;
+                    }
+                    throw new Error(errorMessage)
+                })
+            }
+        }).then(data=>{
+            nameInputRef.current.value=data.users[0].displayName
+            imageInputRef.current.value=data.users[0].photoUrl
+        }).catch(err=>{
+            alert(err.message)
+        })
+    },[authCtx.token])
+
     const onSubmitHandler=(event)=>{
         event.preventDefault();
         const enteredName=nameInputRef.current.value
